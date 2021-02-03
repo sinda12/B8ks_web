c3891f18302eff3203899c5146d7a55f42eb04bb
sinda12 <sghaiersinda@hotmail.com>
2021-02-03 21:12:15 +0100

    crud

--- src/main/java/com/esprit/b8ks_web/services/OrderService.java
+++ src/main/java/com/esprit/b8ks_web/services/OrderService.java
@@ -1,4 +1,8 @@
 package com.esprit.b8ks_web.services;
 
 public class OrderService {
+
+    //afficher random book as sugge
+    //https://stackoverflow.com/questions/50970509/how-to-select-random-10-records-in-jpql
+
 }
